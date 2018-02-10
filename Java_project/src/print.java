8c5e1fa28f8531d99c5c9445868621f6ea31686d
pjsha <pjsha@DESKTOP-LE3VMA5>
2018-02-10 10:52:18 -0600

    First commit:

---
+++ Java_project/src/print.java
@@ -0,0 +1,22 @@
+
+public class print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+String x="john";
+int age=20;
+
+String y="techfios";
+
+
+
+System.out.println("My name is Shaju");
+System.out.println("I am from Texas");		
+System.out.println(x);		
+System.out.println(age);
+System.out.println(y);
+
+	}
+
+}
